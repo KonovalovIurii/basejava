24688c6b3eb19d631763158c7b98e20d6efd78df
KonovalovIurii <konovalov_iurii@yahoo.com>
2020-11-09 13:14:05 +0300

    исправления

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,33 +8,31 @@ import java.util.List;
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10000];
+    //кол-во элементов в массиве
+    Integer size = 0;
 
     void clear() {
-        for (int i = 0; i < 10000; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-
-            }
+        for (int i = 0; i <= size; i++) {
+            storage[i] = null;
         }
+        //сбросим после очистки
+        size = 0;
         return;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < 10000; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                return;
-            }
+        if (size < 10000) {
+            storage[size] = r;
+            size++;
+            return;
         }
-        // Т.к сигнатуры методов менять нельзя выведем простое сообщение в консоль
-        //throw new Exception("Not enough space in the array");
         System.out.println("Not enough space in the array");
         return;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < 10000; i++) {
-            if (storage[i] != null) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             } else {
                 break;
@@ -45,16 +43,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int j = this.size();
-        for (int i = 0; i <= j; i++) {
-            if (storage[i] != null) {
-                if (storage[i].toString() == uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                if (i == size -1) {
+                    //удаляемый элемент является последним
+                    storage[size-1] = null;
+                }else {
                     //переносим последний элемент в удаляемый и затираем последний
-                    storage[i] = storage[j];
-                    storage[j] = null;
+                    storage[i] = storage[size - 1];
+                    storage[size-1] = null;
+                    size--;
                 }
-            } else {
-                break;
             }
         }
     }
@@ -64,20 +63,14 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        if (storage[0] != null) {
-            return storage;
-
-        } else {
-            return new Resume[0];
+        Resume[] outstorage = new Resume[size];
+        for (int i=0; i < size; i++){
+           outstorage[i] =  storage[i];
         }
+        return outstorage;
     }
 
     int size() {
-        for (int i = 0; i < 10000; i++) {
-            if (storage[i] == null) {
-                return i--;
-            }
-        }
-        return 0;
+        return size;
     }
 }
